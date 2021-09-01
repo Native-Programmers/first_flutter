95259f1c8dddc1ae958fde85d4854f5da0bbbc64
nativeprogrammers <nativeprogrammers@gmail.com>
2021-09-01 12:40:02 +0500

    sample text

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: AppBar(
+        title: Text("User Side"),
+      ),
+    );
+  }
+}
